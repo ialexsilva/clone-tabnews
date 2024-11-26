e72341b7a8b3c1a2fe8a9e9ba18efe741307b8f7
Alex Silva <ialexsilva@users.noreply.github.com>
2024-11-26 17:25:36 +0000

    update homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,6 @@
 function Home() {
   return (
     <div>
-      <img
-        src="https://i.imgur.com/XFYxE1G.png"
-        alt="logo"
-        width={800}
-        style={{ display: "block", margin: "auto" }}
-      />
       <h1 style={{ textAlign: "center" }}>Na minha vez?</h1>
     </div>
   );
