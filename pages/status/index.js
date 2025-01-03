ecf6fc1d11b50a0cc25edd28944ff9245b2929d9
Alex Silva <ialexsilva@users.noreply.github.com>
2025-01-03 20:58:22 +0000

    build: add react-icons and config tailwind globals.css

--- pages/status/index.js
+++ pages/status/index.js
@@ -1,4 +1,8 @@
 import useSWR from "swr";
+import { FaRegCheckCircle } from "react-icons/fa";
+import { LuConstruction } from "react-icons/lu";
+import { LuServerCrash } from "react-icons/lu";
+import { LuServerOff } from "react-icons/lu";
 
 async function fetchAPI(key) {
   const response = await fetch(key);
@@ -8,11 +12,53 @@ async function fetchAPI(key) {
 
 export default function StatusPage() {
   return (
-    <>
-      <h1>Status</h1>
-      <UpdatedAt />
+    <div className="w-full lg:px-8 px-4 pt-8">
+      <div className="flex justify-center m-2">
+        <FaRegCheckCircle
+          className="text-green-500"
+          style={{ fontSize: "90px" }}
+        />
+      </div>
+      <div className="flex justify-center mt-6">
+        <h1 className="text-4xl text-center font-bold">
+          Todos os sistemas estão operacionais
+        </h1>
+      </div>
+      <div className="flex flex-col items-start lg:flex-row justify-center mt-6 lg:mt-10">
+        <div className="flex items-center mr-8 mb-4 lg:mb-0">
+          <FaRegCheckCircle
+            className="text-green-500 mr-1"
+            style={{ fontSize: "15px" }}
+          />
+          <p className="text-xs text-center">Nenhum problema</p>
+        </div>
+        <div className="flex items-center mr-8 mb-4 lg:mb-0">
+          <LuConstruction
+            className="text-yellow-600 mr-1"
+            style={{ fontSize: "15px" }}
+          />
+          <p className="text-xs text-center">Manutenção</p>
+        </div>
+        <div className="flex items-center mr-8 mb-4 lg:mb-0">
+          <LuServerCrash
+            className="text-yellow-600 mr-1"
+            style={{ fontSize: "15px" }}
+          />
+          <p className="text-xs text-center">Interrupção parcial</p>
+        </div>
+        <div className="flex items-center mb-4 lg:mb-0">
+          <LuServerOff
+            className="text-red-700 mr-1"
+            style={{ fontSize: "15px" }}
+          />
+          <p className="text-xs text-center">Interrupção</p>
+        </div>
+      </div>
       <DatabaseStatus />
-    </>
+      <div className="justify-center mt-10 mb-10">
+        <UpdatedAt />
+      </div>
+    </div>
   );
 }
 
@@ -27,7 +73,11 @@ function UpdatedAt() {
     updatedAtText = new Date(data.updated_at).toLocaleString("pt-BR");
   }
 
-  return <div>Última atualização: {updatedAtText}</div>;
+  return (
+    <p className="text-center text-gray-400">
+      Última atualização: {updatedAtText}
+    </p>
+  );
 }
 
 function DatabaseStatus() {
@@ -35,26 +85,52 @@ function DatabaseStatus() {
     refreshInterval: 2000,
   });
 
-  let databaseStatusInformation = "Carregando...";
+  let databaseStatusInformation = (
+    <div className="flex justify-center items-center h-full">
+      <p className="text-center text-gray-500">Carregando...</p>
+    </div>
+  );
 
   if (!isLoading && data) {
     databaseStatusInformation = (
-      <>
-        <div>Versão: {data.dependencies.database.version}</div>
-        <div>
-          Conexões abertas: {data.dependencies.database.opened_connections}
+      <div className="card flex flex-wrap mt-4 lg:mt-12 w-full lg:w-3/5 mx-auto">
+        <p className="text-xs flex-row flex-shrink mx-8 mt-7 mb-4 font-semibold text-gray-400">
+          Database
+        </p>
+        <div className="w-full border-t border-1 mx-8 border-gray-50"></div>
+        <div className="w-full p-4">
+          <div className="flex">
+            <div className="flex-col justify-center ml-3">
+              <p className="text-m">
+                Versão: {data.dependencies.database.version}
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          Conexões máximas: {data.dependencies.database.max_connections}
+        <div className="w-full border-t border-1 mx-8 border-gray-50"></div>
+        <div className="w-full p-4">
+          <div className="flex">
+            <div className="flex-col justify-center ml-3">
+              <p className="text-m">
+                Conexões abertas:{" "}
+                {data.dependencies.database.opened_connections}
+              </p>
+            </div>
+          </div>
         </div>
-      </>
+        <div className="w-full border-t border-1 mx-8 border-gray-50"></div>
+        <div className="w-full p-4">
+          <div className="flex">
+            <div className="flex-col justify-center ml-3">
+              <p className="text-m">
+                Conexões máximas: {data.dependencies.database.max_connections}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     );
   }
 
-  return (
-    <>
-      <h2>Database</h2>
-      <div>{databaseStatusInformation}</div>
-    </>
-  );
+  return <>{databaseStatusInformation}</>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Note the addition of the `app` directory.
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 
@@ -11,5 +11,17 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".card": {
+          borderRadius: "0.5rem",
+          overflow: "hidden",
+          backgroundColor: "rgb(255 255 255 / var(--tw-bg-opacity, 1))",
+          boxShadow:
+            "var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), 0 1px 3px 0 rgba(0,0,0,0.1), 0 1px 2px -1px rgba(0,0,0,0.1)",
+        },
+      });
+    },
+  ],
 };
