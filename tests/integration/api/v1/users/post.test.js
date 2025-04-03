8cc78c8dbc948997cbdb7af51d7a0377847bed9f
Alex Silva <ialexsilva@users.noreply.github.com>
2025-04-03 14:39:40 +0000

    feat: add migrator for users table

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,26 @@
+import orchestrator from "tests/orchestrator";
+import database from "infra/database.js";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
+        values: ["alexsilva", "contato@ialexsilva.dev", "senha123"],
+      });
+      const users = await database.query("SELECT * FROM users");
+      console.log(users.rows);
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+
+      expect(response.status).toBe(201);
+    });
+  });
+});
